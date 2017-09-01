c3f3df0987af272570d88800c9241371e346d706
Jacek Bilski <jacek.bilski@zooplus.com>
2017-09-01 17:57:51 +0200

    d3 is preparing svg graph, still simple one.

--- src/angular/app/d3.component.ts
+++ src/angular/app/d3.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
     selector: 'd3',
     templateUrl: './d3.component.html',
+    styleUrls: ['./d3.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class D3Component implements OnInit {
 
@@ -11,8 +13,33 @@ export class D3Component implements OnInit {
     }
 
     ngOnInit() {
-        let body = d3.selectAll("p");
-        let div = body.append("div");
-        div.html("d3p: Hello, world!");
+        const data = [
+            {name: "a", value: 4},
+            {name: "b", value: 8},
+            {name: "c", value: 15},
+            {name: "d", value: 16},
+            {name: "e", value: 23},
+            {name: "fff", value: 42}
+        ];
+        const chartWidth = 500,
+            barHeight = 20;
+        let scale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.value)])
+            .range([0, 500]);
+        let chart = d3.select(".chart")
+            .attr("width", chartWidth)
+            .attr("height", barHeight * data.length);
+        let bar = chart.selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("transform", (d, i) => "translate(0," + i * barHeight + ")");
+        bar.append("rect")
+            .attr("width", d => scale(d.value))
+            .attr("height", barHeight - 1);
+        bar.append("text")
+            .attr("x", d => scale(d.value) - 3)
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(d => d.name);
     }
 }
