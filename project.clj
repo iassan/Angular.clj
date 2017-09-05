960b14d9026febd54710573948564df6b38984d3
Jacek Bilski <jacek.bilski@gmail.com>
2017-09-05 17:31:54 +0200

    Adding data for one investment fund in CSV format and exposing it via JSON.

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  [ring/ring-core "1.6.2"]
                  [ring/ring-jetty-adapter "1.6.2"]
                  [ring/ring-json "0.4.0"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [org.clojure/data.csv "0.1.4"]
+                 ]
   :plugins [[lein-shell "0.5.0"]]
   :main ^:skip-aot api.core
   :target-path "target/%s"
--- src/api/core.clj
+++ src/api/core.clj
@@ -4,7 +4,10 @@
             [ring.middleware.resource :as resource]
             [compojure.core :refer :all]
             [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response :refer :all]))
+            [ring.util.response :refer :all]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            ))
 
 (defn staticHandler [request]
   (content-type (response "Hello, World!") "text/html"))
@@ -12,9 +15,18 @@
 (defn jsonHandler [request]
   (response {:message "Hello, JSON!"}))
 
+(defn tickerHandler [request]
+  (response
+    (with-open [reader (io/reader "data/VQvCQX-investor-obligacji.csv")]
+      (doall (->> (csv/read-csv reader)
+                  rest
+                  (map #(take 2 %))
+                  (map #(hash-map (first %) (second %))))))))
+
 (defroutes my-routes
            (GET "/hello" [] staticHandler)
-           (GET "/json" [] (wrap-json-response jsonHandler)))
+           (GET "/json" [] (wrap-json-response jsonHandler))
+           (GET "/ticker" [] (wrap-json-response tickerHandler)))
 
 (def app
   (resource/wrap-resource my-routes "public"))
