5f64ce0df79c9a53f1e25807802603908d143de5
Jacek Bilski <jacek.bilski@gmail.com>
2017-09-10 20:50:05 +0200

    Pure D3 now also draws line chart with ticker data.

--- src/angular/app/charts.component.ts
+++ src/angular/app/charts.component.ts
@@ -5,20 +5,6 @@ import Chart from 'chart.js/dist/Chart.bundle.js'
 import {AppService} from "./app.service";
 import {TickerValue} from "./ticker-value";
 
-class Value {
-    name: string;
-    value: number;
-}
-
-const data: Value[] = [
-    {name: "a", value: 4},
-    {name: "b", value: 8},
-    {name: "c", value: 15},
-    {name: "d", value: 16},
-    {name: "e", value: 23},
-    {name: "f", value: 42}
-];
-
 @Component({
     selector: 'charts',
     templateUrl: './charts.component.html',
@@ -32,16 +18,16 @@ export class ChartsComponent implements OnInit {
 
     ngOnInit() {
         this.messageService.getTickerValues()
-            .then(d => this.drawCharts(d));
+            .then(d => ChartsComponent.drawCharts(d));
     }
 
-    private drawCharts(tickerData: TickerValue[]) {
-        this.drawUsingPureD3(data);
-        this.drawUsingPlottable(tickerData);
-        this.drawUsingChartJs(tickerData);
+    private static drawCharts(tickerData: TickerValue[]) {
+        ChartsComponent.drawUsingPureD3(tickerData);
+        ChartsComponent.drawUsingPlottable(tickerData);
+        ChartsComponent.drawUsingChartJs(tickerData);
     }
 
-    private drawUsingChartJs(data: TickerValue[]) {
+    private static drawUsingChartJs(data: TickerValue[]) {
         let ctx = document.getElementById("myChartJSChart");
         new Chart.Line(ctx, {
             data: {
@@ -69,47 +55,50 @@ export class ChartsComponent implements OnInit {
         });
     }
 
-    private drawUsingPureD3(data: Value[]) {
+    private static drawUsingPureD3(data: TickerValue[]) {
         const margin = {top: 20, right: 30, bottom: 30, left: 40},
             chartWidth = 800 - margin.left - margin.right,
             chartHeight = 400 - margin.top - margin.bottom;
 
-        let x = d3.scaleBand()
-            .domain(data.map(d => d.name))
-            .range([0, chartWidth])
-            .padding(.1);
+        let x = d3.scaleTime()
+            .domain(d3.extent(data, d => d.date))
+            .range([0, chartWidth]);
         let y = d3.scaleLinear()
-            .range([chartHeight, 0])
-            .domain([0, d3.max(data, d => d.value)]);
+            .domain(d3.extent(data, d => d.value))
+            .range([chartHeight, 0]);
+        let line = d3.line<TickerValue>()
+            .x(d => x(d.date))
+            .y(d => y(d.value));
 
         let chart = d3.select("#myPureD3Chart")
             .attr("width", chartWidth + margin.left + margin.right)
-            .attr("height", chartHeight + margin.top + margin.bottom)
+            .attr("height", chartHeight + margin.top + margin.bottom);
+        let g = chart
             .append("g").attr("transform", "translate(" + margin.left + "," + margin.right + ")");
-        chart.append("g")
+        g.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + chartHeight + ")")
             .call(d3.axisBottom(x));
-        chart.append("g")
+        g.append("g")
             .attr("class", "y axis")
             .call(d3.axisLeft(y));
-        chart.append("text")
+        g.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 6)
             .attr("dy", ".71em")
             .style("text-anchor", "end")
             .text("value");
-        chart.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", d => x(d.name))
-            .attr("y", d => y(d.value))
-            .attr("height", d => chartHeight - y(d.value))
-            .attr("width", x.bandwidth());
+        g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
     }
 
-    private drawUsingPlottable(data: TickerValue[]) {
+    private static drawUsingPlottable(data: TickerValue[]) {
         let xScale = new Plottable.Scales.Time();
         let yScale = new Plottable.Scales.Linear();
 
